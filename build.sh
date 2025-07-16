86303562a2b9f0a5c8524d186234694afad653a7
Ad1tyzzRamadhani <adityafr79@gmail.com>
2025-07-16 14:25:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,21 @@
 
 echo "🔧 Building QueLang compiler..."
 g++ -std=c++17 main.cpp -o quelang
-g++ -std=c++17 dash.cpp -o dash
 chmod +x quelang 
-chmod +x dash
 
 if [ $? -eq 0 ]; then
     echo "✅ Build succeeded: ./quelang"
-    ./dash
+    echo " "
+    echo "=== quelang compiler ==="
+    echo "use :"
+    echo " ./quelang input.q output.s"
+    echo "for debug :"
+    echo " ./quelang --debug input.q output.s"
+    echo " "
+    echo "qc0.7 --Alpha"
 else
     echo "❌ Build failed!"
+    echo "Check https://github.com/Ad1tyzzRamadhani/quelang-"
+    echo "For Latest Fix Info"
     exit 1
 fi
